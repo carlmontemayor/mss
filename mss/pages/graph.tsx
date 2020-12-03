8c3c234f62ce0b5d607f4da1ced1e5ebec6e4e22
Carl Montemayor <cmontemayor@nevada.unr.edu>
2020-12-03 12:49:31 -0800

    Added skeleton of /graph page

--- mss/pages/graph.tsx
+++ mss/pages/graph.tsx
@@ -1,10 +1,37 @@
-import { Button, Grid, Paper, Typography } from '@material-ui/core';
-import { SimpleContainer, ButtonContainer } from 'components/Container';
-import { DefaultGraph } from 'components/Graph';
+import {
+  AppBar,
+  Button,
+  Grid,
+  IconButton,
+  Paper,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import React from 'react';
 
 export const CoolGraph = (): JSX.Element => {
   return (
     <Grid container>
+      <Grid xs={12} item>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">MSS - Microservice Simulation</Typography>
+          </Toolbar>
+        </AppBar>
+      </Grid>
+      <Grid xs={12} item>
+        <Paper>graph</Paper>
+      </Grid>
+      <Grid xs={1} item />
+      <Grid xs={10} item>
+        <Paper>Control Panel</Paper>
+      </Grid>
+      <Grid xs={1} item />
+      <Grid xs={12} item>
+        <Paper>footer</Paper>
+      </Grid>
+
+      {/**
       <Grid xs={12} item>
         <DefaultGraph />
       </Grid>
@@ -16,6 +43,8 @@ export const CoolGraph = (): JSX.Element => {
           <Button variant="contained">MODIFY</Button>
         </SimpleContainer>
       </Grid>
+       * 
+       */}
     </Grid>
   );
 };
