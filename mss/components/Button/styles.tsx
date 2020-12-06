aaf9b18d6767e54764673c1eba47bb16683c47f5
Carl Montemayor <cmontemayor@nevada.unr.edu>
2020-12-06 09:39:11 -0800

    Added functionality for switching simulations and added better type checking

--- mss/components/Button/styles.tsx
+++ mss/components/Button/styles.tsx
@@ -2,6 +2,7 @@ import { styled } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 
 export const ControlPanelButton = styled(Button)({
+  width: '75%',
   color: 'orange',
   border: '1px solid black',
   marginBottom: 20,
--- mss/simulations/index.ts
+++ mss/simulations/index.ts
@@ -1,1 +1,3 @@
-export { BasicSimulation, CoolSimulation } from './1_basic';
+export { CoolSimulation } from './1_basic';
+export { IntricateSimulation } from './2_intricate';
+export { ComplicatedSimulation } from './3_complicated';
