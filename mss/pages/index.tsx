b24d8a62e903328fdb22f093ee21c6ecc30e8a33
Carl Montemayor <cmontemayor@nevada.unr.edu>
2020-12-06 19:10:25 -0800

    Started home page

--- mss/pages/index.tsx
+++ mss/pages/index.tsx
@@ -3,52 +3,42 @@ import Head from 'next/head';
 export const Home = (): JSX.Element => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>MSS</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Welcome to <a href="https://nextjs.org">MSS</a>
       </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.tsx</code>
-      </p>
-
-      <button
-        onClick={() => {
-          window.alert('With typescript and Jest');
-        }}
-      >
-        Test Button
-      </button>
 
       <div className="grid">
         <a href="https://nextjs.org/docs" className="card">
           <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>Find the documentation for the code.</p>
         </a>
 
         <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <h3>Report &rarr;</h3>
+          <p>Read our report regarding our CPE 400 project.</p>
+        </a>
+
+        <a href="https://nextjs.org/learn" className="card">
+          <h3>Problem Statement &rarr;</h3>
+          <p>Read our project description + goals to understand the project.</p>
         </a>
 
         <a
           href="https://github.com/vercel/next.js/tree/master/examples"
           className="card"
         >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <h3>Github Repo &rarr;</h3>
+          <p>Checkout our repository and our repositry here.</p>
         </a>
 
-        <a
-          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+        <a href="/graph" className="card">
+          <h3>Simulation &rarr;</h3>
+          <p>Test our simulation by clicking this card.</p>
         </a>
       </div>
     </main>
@@ -59,7 +49,8 @@ export const Home = (): JSX.Element => (
         target="_blank"
         rel="noopener noreferrer"
       >
-        Powered by <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        Made by Curtis Chung + Carl Montemayor w/ ❤️ <br /> CPE 400 - Dynamic
+        Routing in a Faulty Network
       </a>
     </footer>
 
